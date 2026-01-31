0d8d4ea84f622cea7d7b70eca313c275d4612e99
Teresa Rodríguez Muñoz <teresaromu1999@gmail.com>
2026-01-31 20:59:26 +0100

    Application Quit

--- Assets/_PIMIENTOS_FRITOS/01_Scripts/01_Menu/MainMenuManager.cs
+++ Assets/_PIMIENTOS_FRITOS/01_Scripts/01_Menu/MainMenuManager.cs
@@ -37,6 +37,11 @@ public class MainMenu : MonoBehaviour
 
     public void ExitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+    }
+#endif
     }
 }
