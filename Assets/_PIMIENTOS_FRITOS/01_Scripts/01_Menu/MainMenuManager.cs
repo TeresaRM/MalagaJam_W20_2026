396e6836fc3e1d0104489ec371ff9f798a74fbbf
Teresa Rodríguez Muñoz <teresaromu1999@gmail.com>
2026-01-31 20:29:00 +0100

    Animacion de boton, puertas nuevas, panel de puntuaciones

--- Assets/_PIMIENTOS_FRITOS/01_Scripts/01_Menu/MainMenuManager.cs
+++ Assets/_PIMIENTOS_FRITOS/01_Scripts/01_Menu/MainMenuManager.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] string FreeMode;
     [SerializeField] string HistoryMode;
     [SerializeField] string Gallery;
+    [SerializeField] private CanvasGroup fadeCanvasGroup;
+    private float fadeDuration = 0.5f;
 
     public void GoToFreeMode()
     {
@@ -14,8 +18,18 @@ public class MainMenu : MonoBehaviour
 
     public void GoToHistoryMode()
     {
-        SceneManager.LoadScene(HistoryMode);
+        StartCoroutine(ActionsAfterStartAnim());
+        LeaveMainMenu();
     }
+
+    private IEnumerator ActionsAfterStartAnim()
+    {
+        yield return new WaitForSeconds(fadeDuration);
+        SceneManager.LoadScene(HistoryMode);             // load selected level (lvl 1 = 2, lvl 2 = 3, lvl 3 = 4)
+    }
+
+    private void LeaveMainMenu() => fadeCanvasGroup.DOFade(1, fadeDuration);
+
     public void GoToGallery()
     {
         SceneManager.LoadScene(Gallery);
